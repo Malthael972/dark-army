386485a77a65afe201abfdcf4389deac4e4bfd74
Malthael972 <frankvaliquette123@gmail.com>
2026-01-17 20:45:49 -0700

    Add DarkUnits class with darkStell TankUnitType

---
+++ src/darkarmy/mod/content/DarkUnits.java
@@ -0,0 +1,58 @@
+package darkarmy.mod.content;
+
+import mindustry.type.UnitType;
+import mindustry.content.UnitTypes;
+import mindustry.gen.UnitEntity;
+import mindustry.type.unit.TankUnitType;
+
+public class DarkUnits {
+  public static UnitType darkStell;
+
+  public static void load() {
+
+    darkStell = new TankUnitType("darkStell"){{
+            hitSize = 12f;
+            treadPullOffset = 3;
+            speed = 0.75f;
+            rotateSpeed = 3.5f;
+            health = 850;
+            armor = 6f;
+            itemCapacity = 0;
+            floorMultiplier = 0.95f;
+            treadRects = new Rect[]{new Rect(12 - 32f, 7 - 32f, 14, 51)};
+            researchCostMultiplier = 0f;
+
+            tankMoveVolume *= 0.32f;
+            tankMoveSound = Sounds.tankMoveSmall;
+
+            weapons.add(new Weapon("darkStell-weapon"){{
+                shootSound = Sounds.shootStell;
+                layerOffset = 0.0001f;
+                reload = 50f;
+                shootY = 4.5f;
+                recoil = 1f;
+                rotate = true;
+                rotateSpeed = 2.2f;
+                mirror = false;
+                x = 0f;
+                y = -0.75f;
+                heatColor = Color.valueOf("f9350f");
+                cooldownTime = 30f;
+
+                bullet = new BasicBulletType(4f, 40){{
+                    sprite = "missile-large";
+                    smokeEffect = Fx.shootBigSmoke;
+                    shootEffect = Fx.shootBigColor;
+                    width = 5f;
+                    height = 7f;
+                    lifetime = 40f;
+                    hitSize = 4f;
+                    hitColor = backColor = trailColor = Color.valueOf("feb380");
+                    frontColor = Color.white;
+                    trailWidth = 1.7f;
+                    trailLength = 5;
+                    despawnEffect = hitEffect = Fx.hitBulletColor;
+                }};
+            }});
+        }};
+  }
