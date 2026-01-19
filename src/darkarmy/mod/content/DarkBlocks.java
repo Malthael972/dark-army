47319a39d2dcd116345d7e8e59913fdb14882950
Malthael972 <frankvaliquette123@gmail.com>
2026-01-19 14:18:36 -0600

    Implement darklingFabricator block in DarkBlocks.java
    Added the darklingFabricator block with specifications for unit production.

--- src/darkarmy/mod/content/DarkBlocks.java
+++ src/darkarmy/mod/content/DarkBlocks.java
@@ -1,1 +1,42 @@
 
+package darkarmy.content;
+
+import mindustry.world.*;
+import mindustry.world.blocks.units.*;
+import mindustry.type.*;
+import mindustry.content.*;
+
+import static mindustry.type.ItemStack.*;
+
+public class Blocks {
+
+    public static Block darklingFabricator;
+
+    public static void load(){
+
+        darklingFabricator = new UnitFactory("darkling-fabricator"){{
+            requirements(Category.units,
+                with(
+                    Items.beryllium, 60,
+                    Items.tungsten, 40
+                )
+            );
+
+            size = 3;
+            health = 600;
+
+            consumePower(2.5f);
+
+            plans.add(
+                new UnitPlan(
+                    Units.darkling,
+                    60f * 25f,
+                    with(
+                        Items.beryllium, 30,
+                        Items.tungsten, 20
+                    )
+                )
+            );
+        }};
+    }
+}
