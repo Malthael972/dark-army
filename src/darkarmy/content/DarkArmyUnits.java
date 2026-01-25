21afbfa305e49932ec2a4e60a6bf610ee210a0e5
Malthael972 <frankvaliquette123@gmail.com>
2026-01-24 22:00:13 -0600

    Add DarkArmyUnits class with darkstell unit type

---
+++ src/darkarmy/content/DarkArmyUnits.java
@@ -0,0 +1,107 @@
+packagedarkarmy.content;
+
+import arc.Core;
+import arc.func.Cons;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
+import arc.graphics.g2d.Lines;
+import arc.graphics.g2d.TextureRegion;
+import arc.math.Angles;
+import arc.math.Interp;
+import arc.math.Mathf;
+import arc.math.Rand;
+import arc.math.geom.Rect;
+import arc.math.geom.Vec2;
+import arc.scene.ui.layout.Table;
+import arc.struct.ObjectSet;
+import arc.struct.Seq;
+import arc.util.Time;
+import arc.util.Tmp;
+import mindustry.Vars;
+import mindustry.ai.UnitCommand;
+import mindustry.ai.types.BuilderAI;
+import mindustry.ai.types.FlyingAI;
+import mindustry.ai.types.MinerAI;
+import mindustry.audio.SoundLoop;
+import mindustry.content.Fx;
+import mindustry.content.Items;
+import mindustry.content.StatusEffects;
+import mindustry.entities.;
+import mindustry.entities.abilities.;
+import mindustry.entities.bullet.;
+import mindustry.entities.effect.MultiEffect;
+import mindustry.entities.part.HaloPart;
+import mindustry.entities.part.RegionPart;
+import mindustry.entities.part.ShapePart;
+import mindustry.entities.pattern.;
+import mindustry.entities.units.WeaponMount;
+import mindustry.game.Team;
+import mindustry.gen.*;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
+import mindustry.graphics.MultiPacker;
+import mindustry.graphics.Pal;
+import mindustry.type.StatusEffect;
+import mindustry.type.UnitType;
+import mindustry.type.Weapon;
+import mindustry.type.ammo.ItemAmmoType;
+import mindustry.type.ammo.PowerAmmoType;
+import mindustry.type.weapons.PointDefenseWeapon;
+import mindustry.type.weapons.RepairBeamWeapon;
+import mindustry.world.meta.BlockFlag;
+import mindustry.world.meta.Env;
+
+public class DarkArmyUnits{
+  public static UnitType darkstell;
+
+  public static void load(){
+    darkStell = new TankUnitType("dark-stell"){{
+        constructor = UnitEntity::create;
+      
+            hitSize = 12f;
+            treadPullOffset = 3;
+            speed = 0.75f;
+            rotateSpeed = 3.5f;
+            health = 850;
+            armor = 6f;
+            itemCapacity = 0;
+            floorMultiplier = 0.95f;
+            treadRects = new Rect[]{new Rect(12 - 32f, 7 - 32f, 14, 51)};
+            researchCostMultiplier = 0f;
+
+            tankMoveVolume *= 0.32f;
+            tankMoveSound = Sounds.tankMoveSmall;
+
+            weapons.add(new Weapon("dark-stell-weapon"){{
+                shootSound = Sounds.shootStell;
+                layerOffset = 0.0001f;
+                reload = 50f;
+                shootY = 4.5f;
+                recoil = 1f;
+                rotate = true;
+                rotateSpeed = 2.2f;
+                mirror = false;
+                x = 0f;
+                y = -0.75f;
+                heatColor = Color.valueOf("f9350f");
+                cooldownTime = 30f;
+
+                bullet = new BasicBulletType(4f, 40){{
+                    sprite = "missile-large";
+                    smokeEffect = Fx.shootBigSmoke;
+                    shootEffect = Fx.shootBigColor;
+                    width = 5f;
+                    height = 7f;
+                    lifetime = 40f;
+                    hitSize = 4f;
+                    hitColor = backColor = trailColor = Color.valueOf("feb380");
+                    frontColor = Color.white;
+                    trailWidth = 1.7f;
+                    trailLength = 5;
+                    despawnEffect = hitEffect = Fx.hitBulletColor;
+                }};
+            }});
+        }};
+  }
+}
