c9fe9c46aa41f1e221a426e096c8918edb1da8d8
Malthael972 <frankvaliquette123@gmail.com>
2026-01-25 19:52:53 -0600

    Update DarkArmyTechTree.java

--- src/darkarmy/content/DarkArmyTechTree.java
+++ src/darkarmy/content/DarkArmyTechTree.java
@@ -1,10 +1,8 @@
 package darkarmy.content;
 
-import arc.struct.Seq;
-import mindustry.ctype.UnlockableContent;
 import mindustry.content.Blocks;
-import mindustry.content.UnitTypes;
-import mindustry.game.Objectives.Research;
+import mindustry.content.Planets;
+import mindustry.game.Objectives.OnPlanet;
 
 import static mindustry.content.TechTree.*;
 
@@ -12,47 +10,33 @@ public class DarkArmyTechTree {
 
     public static void load() {
 
-        // ========= UNITS =========
-        addAfter(UnitTypes.stell,    DarkArmyUnits.darkStell);
-        addAfter(UnitTypes.locus,    DarkArmyUnits.darkLocus);
-        addAfter(UnitTypes.merui,    DarkArmyUnits.darkMerui);
-        addAfter(UnitTypes.cleroi,   DarkArmyUnits.darkCleroi);
-        addAfter(UnitTypes.elude,    DarkArmyUnits.darkElude);
-        addAfter(UnitTypes.avert,    DarkArmyUnits.darkAvert);
-        addAfter(UnitTypes.obviate,  DarkArmyUnits.darkObviate);
-        addAfter(UnitTypes.quell,    DarkArmyUnits.darkQuell);
-        addAfter(UnitTypes.disrupt,  DarkArmyUnits.darkDisrupt);
-        addAfter(UnitTypes.precept,  DarkArmyUnits.darkPrecept);
-        addAfter(UnitTypes.anthicus, DarkArmyUnits.darkAnthicus);
+        // Attach to Core Bastion (Erekir core)
+        node(Blocks.coreBastion, () -> {
 
-        // ========= BLOCKS =========
-        addAfter(Blocks.tankFabricator,    DarkArmyBlocks.darkTankFabricator);
-        addAfter(Blocks.tankRefabricator,  DarkArmyBlocks.darkTankRefabricator);
-        addAfter(Blocks.mechFabricator,    DarkArmyBlocks.darkMechFabricator);
-        addAfter(Blocks.mechRefabricator,  DarkArmyBlocks.darkMechRefabricator);
-        addAfter(Blocks.shipFabricator,    DarkArmyBlocks.darkShipFabricator);
-        addAfter(Blocks.shipRefabricator,  DarkArmyBlocks.darkShipRefabricator);
-        addAfter(Blocks.primeRefabricator, DarkArmyBlocks.darkPrimeRefabricator);
-        addAfter(Blocks.turbineCondenser,  DarkArmyBlocks.darkTurbineCondenser);
-        addAfter(Blocks.beamNode,          DarkArmyBlocks.darkNode);
-    }
+            // === DARK ARMY ROOT ===
+            node(DarkArmyBlocks.darkBreach, () -> {
 
-    private static void addAfter(UnlockableContent vanilla, UnlockableContent mod) {
+                // ---- BLOCKS ----
+                node(DarkArmyBlocks.darkTankFabricator, () -> {});
+                node(DarkArmyBlocks.darkTankRefabricator, () -> {});
+                node(DarkArmyBlocks.darkMechFabricator, () -> {});
+                node(DarkArmyBlocks.darkMechRefabricator, () -> {});
+                node(DarkArmyBlocks.darkShipFabricator, () -> {});
+                node(DarkArmyBlocks.darkShipRefabricator, () -> {});
+                node(DarkArmyBlocks.darkPrimeRefabricator, () -> {});
+                node(DarkArmyBlocks.darkTurbineCondenser, () -> {});
+                node(DarkArmyBlocks.darkNode, () -> {});
+            });
 
-        if (vanilla.techNode == null) {
-            throw new RuntimeException(
-                "TechNode missing for vanilla content: " + vanilla.name +
-                " (check load order)"
-            );
-        }
-
-        node(vanilla, () -> {
-            node(
-                mod,
-                vanilla.researchRequirements(),
-                Seq.with(new Research(vanilla)),
-                () -> {}
-            );
+            // ---- UNITS ----
+            node(DarkArmyUnits.darkStell, () -> {});
+            node(DarkArmyUnits.darkLocus, () -> {});
+            node(DarkArmyUnits.darkMerui, () -> {});
+            node(DarkArmyUnits.darkCleroi, () -> {});
+            node(DarkArmyUnits.darkQuell, () -> {});
+            node(DarkArmyUnits.darkDisrupt, () -> {});
+            node(DarkArmyUnits.darkPrecept, () -> {});
+            node(DarkArmyUnits.darkAnthicus, () -> {});
         });
     }
 }
