65054085a524015dae049041de61d0bfcb8c7337
Malthael972 <frankvaliquette123@gmail.com>
2026-01-25 20:46:14 -0600

    Update DarkArmyTechTree.java

--- src/darkarmy/content/DarkArmyTechTree.java
+++ src/darkarmy/content/DarkArmyTechTree.java
@@ -1,75 +1,114 @@
 package darkarmy.content;
 
 import mindustry.content.*;
+import mindustry.game.Objectives;
+import mindustry.type.SectorPreset;
+
 import static mindustry.content.TechTree.*;
 
 public class DarkArmyTechTree {
 
     public static void load() {
-        // These attach to EXISTING nodes in the vanilla tree
-        // The node() call adds your content as a child
-        
-        node(Blocks.beamNode, () -> {
-            node(DarkArmyBlocks.darkNode, () -> {});
-            node(DarkArmyBlocks.darkTurbineCondenser, () -> {});
+
+        /* ----------------------------
+           CORE BRANCH
+        -----------------------------*/
+
+        node(Blocks.coreBastion, () -> {
+
+            node(DarkArmyBlocks.darkCore, () -> {
+                objectives.add(
+                    new Objectives.SectorComplete(SectorPresets.basaltFields)
+                );
+            });
         });
-        
+
+        /* ----------------------------
+           TANK LINE
+        -----------------------------*/
+
         node(Blocks.tankFabricator, () -> {
+
+            // Dark Tank Fabricator
             node(DarkArmyBlocks.darkTankFabricator, () -> {
-                node(DarkArmyUnits.darkStell, () -> {});
+                objectives.add(
+                    new Objectives.SectorComplete(SectorPresets.basaltFields)
+                );
+
+                // T1 Tank
+                node(DarkArmyUnits.darkStell, () -> {
+                    objectives.add(
+                        new Objectives.Research(DarkArmyBlocks.darkTankFabricator)
+                    );
+                });
             });
         });
-        
+
+        // Refabricator (T2)
         node(Blocks.tankRefabricator, () -> {
+
             node(DarkArmyBlocks.darkTankRefabricator, () -> {
+                objectives.add(
+                    new Objectives.Research(DarkArmyBlocks.darkTankFabricator)
+                );
+
                 node(DarkArmyUnits.darkLocus, () -> {
-                    node(DarkArmyUnits.darkPrecept, () -> {
-                        node(DarkArmyUnits.darkVanquish, () -> {
-                            node(DarkArmyUnits.darkConquer, () -> {});
-                        });
-                    });
+                    objectives.add(
+                        new Objectives.Research(DarkArmyBlocks.darkTankRefabricator)
+                    );
                 });
             });
         });
-        
+
+        /* ----------------------------
+           MECH LINE
+        -----------------------------*/
+
         node(Blocks.mechFabricator, () -> {
+
             node(DarkArmyBlocks.darkMechFabricator, () -> {
-                node(DarkArmyUnits.darkMerui, () -> {});
-            });
-        });
-        
-        node(Blocks.mechRefabricator, () -> {
-            node(DarkArmyBlocks.darkMechRefabricator, () -> {
-                node(DarkArmyUnits.darkCleroi, () -> {
-                    node(DarkArmyUnits.darkAnthicus, () -> {
-                        node(DarkArmyUnits.darkTecta, () -> {
-                            node(DarkArmyUnits.darkCollaris, () -> {});
-                        });
-                    });
+                objectives.add(
+                    new Objectives.SectorComplete(SectorPresets.oxideFields)
+                );
+
+                node(DarkArmyUnits.darkPrecept, () -> {
+                    objectives.add(
+                        new Objectives.Research(DarkArmyBlocks.darkMechFabricator)
+                    );
                 });
             });
         });
-        
+
+        /* ----------------------------
+           SHIP LINE
+        -----------------------------*/
+
         node(Blocks.shipFabricator, () -> {
+
             node(DarkArmyBlocks.darkShipFabricator, () -> {
-                node(DarkArmyUnits.darkElude, () -> {});
-            });
-        });
-        
-        node(Blocks.shipRefabricator, () -> {
-            node(DarkArmyBlocks.darkShipRefabricator, () -> {
-                node(DarkArmyUnits.darkAvert, () -> {
-                    node(DarkArmyUnits.darkObviate, () -> {
-                        node(DarkArmyUnits.darkQuell, () -> {
-                            node(DarkArmyUnits.darkDisrupt, () -> {});
-                        });
-                    });
+                objectives.add(
+                    new Objectives.SectorComplete(SectorPresets.carbideCliffs)
+                );
+
+                node(DarkArmyUnits.darkVanquish, () -> {
+                    objectives.add(
+                        new Objectives.Research(DarkArmyBlocks.darkShipFabricator)
+                    );
                 });
             });
         });
-        
+
+        /* ----------------------------
+           ENDGAME UNIT
+        -----------------------------*/
+
         node(Blocks.primeRefabricator, () -> {
-            node(DarkArmyBlocks.darkPrimeRefabricator, () -> {});
+
+            node(DarkArmyUnits.darkConquer, () -> {
+                objectives.add(
+                    new Objectives.Research(Blocks.primeRefabricator)
+                );
+            });
         });
     }
 }
