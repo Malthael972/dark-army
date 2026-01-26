259d02c2d9352ebcf2cb7afd0330b8bdcea5ada9
Malthael972 <frankvaliquette123@gmail.com>
2026-01-25 21:46:29 -0600

    Update DarkArmyTechTree.java

--- src/darkarmy/content/DarkArmyTechTree.java
+++ src/darkarmy/content/DarkArmyTechTree.java
@@ -1,6 +1,7 @@
 package darkarmy.content;
 
 import mindustry.content.Blocks;
+import mindustry.content.Items;
 import mindustry.content.TechTree;
 import mindustry.type.ItemStack;
 
@@ -10,27 +11,23 @@ public class DarkArmyTechTree {
 
     public static void load() {
 
-        TechTree.TechNode bastion = TechTree.get(Blocks.coreBastion);
-
-        if (bastion == null) {
-            throw new RuntimeException("Core Bastion tech node not found");
-        }
-
-        node(bastion, DarkArmyUnits.darkStell, ItemStack.with(
-                mindustry.content.Items.beryllium, 120,
-                mindustry.content.Items.tungsten, 80,
-                mindustry.content.Items.silicon, 60
+        // === UNITS ===
+        node(Blocks.coreBastion, DarkArmyUnits.darkStell, ItemStack.with(
+                Items.beryllium, 120,
+                Items.tungsten, 80,
+                Items.silicon, 60
         ), () -> {});
 
-        node(bastion, DarkArmyBlocks.darkTankFabricator, ItemStack.with(
-                mindustry.content.Items.beryllium, 200,
-                mindustry.content.Items.tungsten, 150,
-                mindustry.content.Items.oxide, 50
+        // === BLOCKS ===
+        node(Blocks.coreBastion, DarkArmyBlocks.darkTankFabricator, ItemStack.with(
+                Items.beryllium, 200,
+                Items.tungsten, 150,
+                Items.oxide, 50
         ), () -> {
 
             node(DarkArmyBlocks.darkTankRefabricator, ItemStack.with(
-                    mindustry.content.Items.tungsten, 250,
-                    mindustry.content.Items.carbide, 120
+                    Items.tungsten, 250,
+                    Items.carbide, 120
             ), () -> {});
         });
     }
