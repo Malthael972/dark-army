bb125e89d9860b91486fdf8b81694249a2746b73
Malthael972 <frankvaliquette123@gmail.com>
2026-01-25 19:11:40 -0700

    Update DarkArmyTechTree.java

--- src/darkarmy/content/DarkArmyTechTree.java
+++ src/darkarmy/content/DarkArmyTechTree.java
@@ -1,42 +1,76 @@
 package darkarmy.content;
 
 import mindustry.content.Blocks;
-import mindustry.content.Planets;
-import mindustry.game.Objectives.OnPlanet;
+import mindustry.content.UnitTypes;
+import mindustry.game.Objectives.*;
 
 import static mindustry.content.TechTree.*;
 
 public class DarkArmyTechTree {
 
     public static void load() {
-
-        // Attach to Core Bastion (Erekir core)
-        node(Blocks.coreBastion, () -> {
-
-            // === DARK ARMY ROOT ===
-            node(DarkArmyBlocks.darkNode, () -> {
-
-                // ---- BLOCKS ----
-                node(DarkArmyBlocks.darkTankFabricator, () -> {});
-                node(DarkArmyBlocks.darkTankRefabricator, () -> {});
-                node(DarkArmyBlocks.darkMechFabricator, () -> {});
-                node(DarkArmyBlocks.darkMechRefabricator, () -> {});
-                node(DarkArmyBlocks.darkShipFabricator, () -> {});
-                node(DarkArmyBlocks.darkShipRefabricator, () -> {});
-                node(DarkArmyBlocks.darkPrimeRefabricator, () -> {});
-                node(DarkArmyBlocks.darkTurbineCondenser, () -> {});
         
+        // Add your power blocks to vanilla power tree
+        node(Blocks.beamNode, () -> {
+            node(DarkArmyBlocks.darkNode, () -> {
+                node(DarkArmyBlocks.darkTurbineCondenser, () -> {});
             });
-
-            // ---- UNITS ----
-            node(DarkArmyUnits.darkStell, () -> {});
-            node(DarkArmyUnits.darkLocus, () -> {});
-            node(DarkArmyUnits.darkMerui, () -> {});
-            node(DarkArmyUnits.darkCleroi, () -> {});
-            node(DarkArmyUnits.darkQuell, () -> {});
-            node(DarkArmyUnits.darkDisrupt, () -> {});
-            node(DarkArmyUnits.darkPrecept, () -> {});
-            node(DarkArmyUnits.darkAnthicus, () -> {});
+        });
+        
+        // Add your tank line after vanilla tank fabricator
+        node(Blocks.tankFabricator, () -> {
+            node(DarkArmyBlocks.darkTankFabricator, () -> {
+                node(DarkArmyUnits.darkStell, () -> {
+                    node(DarkArmyBlocks.darkTankRefabricator, () -> {
+                        node(DarkArmyUnits.darkLocus, () -> {
+                            node(DarkArmyUnits.darkPrecept, () -> {
+                                node(DarkArmyUnits.darkVanquish, () -> {
+                                    node(DarkArmyUnits.darkConquer, () -> {});
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+        
+        // Add your mech line after vanilla mech fabricator
+        node(Blocks.mechFabricator, () -> {
+            node(DarkArmyBlocks.darkMechFabricator, () -> {
+                node(DarkArmyUnits.darkMerui, () -> {
+                    node(DarkArmyBlocks.darkMechRefabricator, () -> {
+                        node(DarkArmyUnits.darkCleroi, () -> {
+                            node(DarkArmyUnits.darkAnthicus, () -> {
+                                node(DarkArmyUnits.darkTecta, () -> {
+                                    node(DarkArmyUnits.darkCollaris, () -> {});
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+        
+        // Add your ship line after vanilla ship fabricator
+        node(Blocks.shipFabricator, () -> {
+            node(DarkArmyBlocks.darkShipFabricator, () -> {
+                node(DarkArmyUnits.darkElude, () -> {
+                    node(DarkArmyBlocks.darkShipRefabricator, () -> {
+                        node(DarkArmyUnits.darkAvert, () -> {
+                            node(DarkArmyUnits.darkObviate, () -> {
+                                node(DarkArmyUnits.darkQuell, () -> {
+                                    node(DarkArmyUnits.darkDisrupt, () -> {});
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+        
+        // Add prime refabricator after vanilla prime refabricator
+        node(Blocks.primeRefabricator, () -> {
+            node(DarkArmyBlocks.darkPrimeRefabricator, () -> {});
         });
     }
 }
