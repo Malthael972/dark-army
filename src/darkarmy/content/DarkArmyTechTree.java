a6c3defba44bbc95de6440af8b93845bf4a9843b
Malthael972 <frankvaliquette123@gmail.com>
2026-01-25 17:42:04 -0700

    Update DarkArmyTechTree.java

--- src/darkarmy/content/DarkArmyTechTree.java
+++ src/darkarmy/content/DarkArmyTechTree.java
@@ -5,48 +5,64 @@ import mindustry.content.TechTree.*;
 
 import static mindustry.content.Blocks.*;
 import static mindustry.content.UnitTypes.*;
+import static mindustry.content.TechTree.*;
 
 public class DarkArmyTechTree {
     public static void load() {
-        // Tank Units - parallel to vanilla tank line
-        addToNode(stell, DarkArmyUnits.darkStell);
-        addToNode(locus, DarkArmyUnits.darkLocus);
-        addToNode(precept, DarkArmyUnits.darkPrecept);
-        addToNode(vanquish, DarkArmyUnits.darkVanquish);
-        addToNode(conquer, DarkArmyUnits.darkConquer);
+        // Find the Erekir planet's tech tree root and add nodes to it
+        TechNode erekirRoot = TechTree.all.find(t -> t.content == Planets.erekir);
         
-        // Mech Units - parallel to vanilla mech line
-        addToNode(merui, DarkArmyUnits.darkMerui);
-        addToNode(cleroi, DarkArmyUnits.darkCleroi);
-        addToNode(anthicus, DarkArmyUnits.darkAnthicus);
-        addToNode(tecta, DarkArmyUnits.darkTecta);
-        addToNode(collaris, DarkArmyUnits.darkCollaris);
-        
-        // Ship Units - parallel to vanilla ship line
-        addToNode(elude, DarkArmyUnits.darkElude);
-        addToNode(avert, DarkArmyUnits.darkAvert);
-        addToNode(obviate, DarkArmyUnits.darkObviate);
-        addToNode(quell, DarkArmyUnits.darkQuell);
-        addToNode(disrupt, DarkArmyUnits.darkDisrupt);
-        
-        // Production Blocks - parallel to vanilla fabricators
-        addToNode(tankFabricator, DarkArmyBlocks.darkTankFabricator);
-        addToNode(DarkArmyBlocks.darkTankFabricator, DarkArmyBlocks.darkTankRefabricator);
-        
-        addToNode(mechFabricator, DarkArmyBlocks.darkMechFabricator);
-        addToNode(DarkArmyBlocks.darkMechFabricator, DarkArmyBlocks.darkMechRefabricator);
-        
-        addToNode(shipFabricator, DarkArmyBlocks.darkShipFabricator);
-        addToNode(DarkArmyBlocks.darkShipFabricator, DarkArmyBlocks.darkShipRefabricator);
-        
-        addToNode(primeRefabricator, DarkArmyBlocks.darkPrimeRefabricator);
+        if(erekirRoot != null) {
+            // Set context to the Erekir root temporarily
+            TechNode prevContext = context();
+            
+            // Tank Units - add as children of vanilla tank units
+            addAfter(stell, DarkArmyUnits.darkStell);
+            addAfter(locus, DarkArmyUnits.darkLocus);
+            addAfter(precept, DarkArmyUnits.darkPrecept);
+            addAfter(vanquish, DarkArmyUnits.darkVanquish);
+            addAfter(conquer, DarkArmyUnits.darkConquer);
+            
+            // Mech Units - add as children of vanilla mech units
+            addAfter(merui, DarkArmyUnits.darkMerui);
+            addAfter(cleroi, DarkArmyUnits.darkCleroi);
+            addAfter(anthicus, DarkArmyUnits.darkAnthicus);
+            addAfter(tecta, DarkArmyUnits.darkTecta);
+            addAfter(collaris, DarkArmyUnits.darkCollaris);
+            
+            // Ship Units - add as children of vanilla ship units
+            addAfter(elude, DarkArmyUnits.darkElude);
+            addAfter(avert, DarkArmyUnits.darkAvert);
+            addAfter(obviate, DarkArmyUnits.darkObviate);
+            addAfter(quell, DarkArmyUnits.darkQuell);
+            addAfter(disrupt, DarkArmyUnits.darkDisrupt);
+            
+            // Production Blocks - add as children of vanilla fabricators
+            addAfter(tankFabricator, DarkArmyBlocks.darkTankFabricator);
+            addAfter(DarkArmyBlocks.darkTankFabricator, DarkArmyBlocks.darkTankRefabricator);
+            
+            addAfter(mechFabricator, DarkArmyBlocks.darkMechFabricator);
+            addAfter(DarkArmyBlocks.darkMechFabricator, DarkArmyBlocks.darkMechRefabricator);
+            
+            addAfter(shipFabricator, DarkArmyBlocks.darkShipFabricator);
+            addAfter(DarkArmyBlocks.darkShipFabricator, DarkArmyBlocks.darkShipRefabricator);
+            
+            addAfter(primeRefabricator, DarkArmyBlocks.darkPrimeRefabricator);
+        }
     }
     
-    private static void addToNode(mindustry.ctype.UnlockableContent parent, mindustry.ctype.UnlockableContent child) {
+    /** Adds a node as a child of the parent content's tech node. */
+    private static void addAfter(mindustry.ctype.UnlockableContent parent, mindustry.ctype.UnlockableContent child) {
+        // Find the parent's tech node
         TechNode parentNode = TechTree.all.find(t -> t.content == parent);
         if(parentNode == null) return;
         
+        // Create a new tech node as a child of the parent
         TechNode node = new TechNode(parentNode, child, child.researchRequirements());
-        parentNode.children.add(node);
+        
+        // Copy research cost multipliers from parent if they exist
+        if(parentNode.researchCostMultipliers != null) {
+            node.researchCostMultipliers = parentNode.researchCostMultipliers;
+        }
     }
 }
