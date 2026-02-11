fb85d49583380a7b945e79d2ed57df45df64b09a
Malthael972 <frankvaliquette123@gmail.com>
2026-02-10 21:24:57 -0600

    Update DarkArmyUnitRegions.java

--- src/darkarmy/content/DarkArmyUnitRegions.java
+++ src/darkarmy/content/DarkArmyUnitRegions.java
@@ -1,1 +1,33 @@
+package darkarmy.content;
 
+import arc.Core;
+import arc.Events;
+import arc.util.Log;
+
+import mindustry.game.EventType;
+
+public class DarkUnitRegions {
+
+    public static void load() {
+
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+
+            DarkUnits.darkStell.region =
+                Core.atlas.find("dark-stell");
+
+            DarkUnits.darkStell.cellRegion =
+                Core.atlas.find("dark-stell-cell");
+
+            DarkUnits.darkStell.treadRegion =
+                Core.atlas.find("dark-stell-treads");
+
+            DarkUnits.darkStell.shadowRegion =
+                Core.atlas.find("dark-stell-shadow");
+
+            // weapons
+            DarkUnits.darkStell.weapons.each(w ->
+                w.region = Core.atlas.find(w.name)
+            );
+        });
+    }
+}
